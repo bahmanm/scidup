170eff6e4ffe93d1427eb08d11118238fbd13009
Bahman Movaqar <Bahman@BahmanM.com>
2026-05-07 11:03:38 -0700

    Move Game state accessors to staging

--- libscidup/libs/database/src/game.cpp
+++ libscidup/libs/database/src/game.cpp
@@ -14,56 +14,10 @@
 
 #include "scidup/database/game.h"
 #include "scidup/database/common.h"
-#include "movetree.h"
 #include <algorithm>
 #include <cstring>
 
 namespace scid::database {
-
-simpleMoveT* Game::GetCurrentMove() {
-	return CurrentMove->endMarker() ? nullptr : &CurrentMove->moveData;
-}
-
-uint Game::GetNumVariations() const {
-	return CurrentMove->numVariations;
-}
-
-uint Game::GetVarNumber() const {
-	if (VarDepth != 0) {
-		uint varNumber = 0;
-		auto moves = CurrentMove->getParent();
-		for (auto parent = moves.first; parent; varNumber++) {
-			parent = parent->varChild;
-			if (parent == moves.second)
-				return varNumber;
-		}
-	}
-	return 0;
-}
-
-bool Game::AtVarStart() const {
-	return CurrentMove->prev->startMarker();
-}
-
-bool Game::AtVarEnd() const {
-	return CurrentMove->endMarker();
-}
-
-bool Game::AtStart() const {
-	return VarDepth == 0 && AtVarStart();
-}
-
-bool Game::AtEnd() const {
-	return VarDepth == 0 && AtVarEnd();
-}
-
-bool Game::AtEmptyVar() const {
-	return VarDepth != 0 && AtVarStart() && AtVarEnd();
-}
-
-//////////////////////////////////////////////////////////////////////
-//  PUBLIC FUNCTIONS
-//////////////////////////////////////////////////////////////////////
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Game::FindExtraTag():
---
+++ libscidup/libs/database/src/game_TEMP/state.cpp
@@ -0,0 +1,48 @@
+#include "scidup/database/game.h"
+
+#include "movetree.h"
+
+namespace scid::database {
+
+simpleMoveT* Game::GetCurrentMove() {
+	return CurrentMove->endMarker() ? nullptr : &CurrentMove->moveData;
+}
+
+uint Game::GetNumVariations() const {
+	return CurrentMove->numVariations;
+}
+
+uint Game::GetVarNumber() const {
+	if (VarDepth != 0) {
+		uint varNumber = 0;
+		auto moves = CurrentMove->getParent();
+		for (auto parent = moves.first; parent; varNumber++) {
+			parent = parent->varChild;
+			if (parent == moves.second)
+				return varNumber;
+		}
+	}
+	return 0;
+}
+
+bool Game::AtVarStart() const {
+	return CurrentMove->prev->startMarker();
+}
+
+bool Game::AtVarEnd() const {
+	return CurrentMove->endMarker();
+}
+
+bool Game::AtStart() const {
+	return VarDepth == 0 && AtVarStart();
+}
+
+bool Game::AtEnd() const {
+	return VarDepth == 0 && AtVarEnd();
+}
+
+bool Game::AtEmptyVar() const {
+	return VarDepth != 0 && AtVarStart() && AtVarEnd();
+}
+
+} // namespace scid::database
