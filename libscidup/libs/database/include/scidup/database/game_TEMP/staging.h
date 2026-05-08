3ef935ff076b1b57ea0842b032645e1f74584f04
Bahman Movaqar <Bahman@BahmanM.com>
2026-05-08 14:00:43 -0700

    Remove unused header

--- libscidup/libs/database/include/scidup/database/game_TEMP/staging.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-// Temporary staging area for decomposing scid::database::Game.
-// Headers under game_TEMP are migration-only and should either move to their
-// final home or be deleted as the core::Game split progresses.
-
